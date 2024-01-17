2f2b7d3b3f0d812d27bd870f90c287499e7faee2
m-waseem <iamwaseem0066@gmail.com>
2024-01-17 22:59:29 +0530

    updated code

--- src/components/Work/CurtinOpenday.js
+++ src/components/Work/CurtinOpenday.js
@@ -18,33 +18,21 @@ const CurtinOpenDay = () => {
   };
 
   return (
-    <div className={`page_homepage__section__S9KCY   WorkGrid_work-grid__0043M page_work-grid__VoMxE   WorkGrid_work-grid__row__p9SUz WorkGrid_work-grid__row--landscape__klf5g ${isSidebarOpen ? 'sidebar-open' : ''}`}>
+    <div className="WorkGrid_work-grid__row__p9SUz WorkGrid_work-grid__row--landscape__klf5g">
       <div className="WorkCard_work-card-wrapper__7mGrZ" style={{ '--aspect-x': 1452, '--aspect-y': 890, opacity: 1, transform: 'translateY(0px)' }}>
         <Link to={`${path}`} className="WorkCard_work-card__Z7y63 WorkCard_work-card--landscape__cw0_a" onClick={handleCardClick}>
           <div className="WorkCard_work-card__thumbnail-wrapper__DZTs2">
             <div className="WorkCard_work-card__thumbnail-outer__kJvp9" style={{ transform: 'translateY(-50%) translateZ(0px)' }}>
-              {/* Vimeo Video */}
-              <video
-                className="ProjectContents_project-content__video__OGkYT"
-                autoPlay
-                playsInline
-                loop
-                preload="none"
-                aria-describedby="video-description-0-0"
-                style={{paddingTop:"10px"}}
-              >
-                <source
-                  src="https://player.vimeo.com/progressive_redirect/playback/895729829/rendition/720p/file.mp4?loc=external&amp;log_user=0&amp;signature=a4af87be7154edb7c1c1a2ebbc454f2c9ca39ce762345f43ff4bf48a898335d8"
-                  type="video/mp4"
-                />
-                Your browser does not support the video tag.
-              </video>
+              <picture className="Picture_picture__X3Eos WorkCard_work-card__picture__CqjRI">
+                <source srcSet="https://a-us.storyblok.com/f/1017006/3720x2280/c7c8f88227/curtin-open-landscape-card.jpg?filters:quality(80)" media="(min-width: 0px) and (max-width: 479px)" />
+                <img src="https://a-us.storyblok.com/f/1017006/3720x2280/c7c8f88227/curtin-open-landscape-card.jpg?filters:quality(80)" loading="lazy" width="450" height="330" alt="" className="" draggable="false" />
+              </picture>
             </div>
             <div className="WorkCard_work-card__thumbnail-inner__YScRN">
-              {/* <picture className="Picture_picture__X3Eos WorkCard_work-card__picture__CqjRI">
-                <source srcSet="https://a-us.storyblok.com/f/1017006/1476x843/7c8bacd5b5/open-day-inner.jpg/m/312x178/filters:quality(80) 1x, https://a-us.storyblok.com/f/1017006/1476x843/7c8bacd5b5/open-day-inner.jpg/m/624x356/filters:quality(80) 2x" media="(min-width: 0px) and (max-width: 479px)" />
-                <img src="https://a-us.storyblok.com/f/1017006/1476x843/7c8bacd5b5/open-day-inner.jpg/m/312x178/filters:quality(80)" loading="lazy" width="312" height="178" alt="Screenshot of the Curtin Open Day website" className="" draggable="false" />
-              </picture> */}
+              <picture className="Picture_picture__X3Eos WorkCard_work-card__picture__CqjRI">
+                <source srcSet="https://a-us.storyblok.com/f/1017006/1476x843/7c8bacd5b5/open-day-inner.jpg?filters:quality(80)" media="(min-width: 0px) and (max-width: 479px)" />
+                <img src="https://a-us.storyblok.com/f/1017006/1476x843/7c8bacd5b5/open-day-inner.jpg?filters:quality(80)" loading="lazy" width="312" height="178" alt="Screenshot of the Curtin Open Day website" className="" draggable="false" />
+              </picture>
             </div>
           </div>
           <div className="WorkCard_work-card__content__Br9N4 WorkCard_work-card__content--white__ISlcm">
